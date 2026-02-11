03ea18f317159ad4a29588645cc0b5abac01a42f
kirpich <yuliakirp@mail.ru>
2026-02-11 09:45:56 +0000

    da

--- 1.peremen/7.c
+++ 1.peremen/7.c
@@ -7,8 +7,8 @@ int main() {
     
     char ch = 'A' + rand() % 26;
     int i = rand();
-    float f = (float)rand() / RAND_MAX;
-    double d = (double)rand() / RAND_MAX;
+    float f = (float)rand();
+    double d = (double)rand();
     
     printf("Значения: символ: %c, число: %d, число с плавающей точкой: %f, число с двойной точностью: %f\n",
            ch, i, f, d);
